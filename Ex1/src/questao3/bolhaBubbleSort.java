276e96f8a43a6cafbd5a87087793ddeec48f5ef5
acannee <57539101+acannee@users.noreply.github.com>
2022-09-20 11:10:30 -0300

    Add files via upload

--- Ex1/src/questao3/bolhaBubbleSort.java
+++ Ex1/src/questao3/bolhaBubbleSort.java
@@ -1,10 +1,33 @@
 package questao3;
 
+import java.util.Random;
+
+
 public class bolhaBubbleSort {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Random numrandomico = new Random();
+	int aux=0;
+	int i=0;
+	int vetor[];
+	vetor = new int[5];
+	for(i= 0;i<5;i++){
+		vetor[i]= numrandomico.nextInt(99);				
+		System.out.println(" "+vetor[i]);	
+	}
+	System.out.println(" ");
+	
+	for(i = 0; i<vetor.length; i++){
+		for(int j = 0; j<vetor.length-1; j++){
+			if(vetor[j] > vetor[j + 1]){
+				aux = vetor[j];
+				vetor[j] = vetor[j+1];
+				vetor[j+1] = aux;
+			}
+		}
+	}
+		System.out.println("Vetor organizado:");
+		for(i = 0; i<5; i++){
+			System.out.println(" "+vetor[i]);
+		}
 	}
 
 }
